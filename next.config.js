9bdf964a44e054baaec4070ea668c98a0e69dd46
Andy Randrianirina <andy.randrianirina@gmail.com>
2021-08-10 23:25:57 +0400

    Updating API URL

--- next.config.js
+++ next.config.js
@@ -1,3 +1,7 @@
+if (process.env.NODE_ENV === 'development') {
+  require('dotenv').config()
+}
+
 module.exports = {
     async redirects() {
         return [
@@ -5,7 +9,11 @@ module.exports = {
             source: '/',
             destination: '/matches',
             permanent: true,
+            env: {
+              API_URL: process.env.API_URL,
+            }
           },
         ]
       },
 }
+
